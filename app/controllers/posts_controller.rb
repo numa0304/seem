1766562fab6fa07255aaa956e3ba7f035b94ce14
chinamiyamauchi <chinami0370@gmail.com>
2017-03-12 09:23:13 +0000

    コントローラの変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,13 @@
 class PostsController < ApplicationController
+  before_action :set_post, only:[:show, :edit, :destroy]
   def new
+    @post = Post.new
   end
 
   def create
-    @post = Post.new
-    @post.text = params[:text]
+    @post = Post.new(post_params)
     @post.save 
-    redirect_to post_path(@post.id)
+    redirect_to @post
   end
 
   def index
@@ -14,6 +15,21 @@ class PostsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit   
+  end
+
+  def update
+    @post.update(post_params)
+    redirect_to @post
+  end
+
+private
+  def set_post
     @post = Post.find(params[:id])
+  end
+  def post_params
+    params.require(:post).permit(:text)
   end
 end
