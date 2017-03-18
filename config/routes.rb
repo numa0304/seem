1146eb9a7eccabfddb1eeb5dda5fdf2d14766058
chinamiyamauchi <chinami0370@gmail.com>
2017-03-18 04:18:41 +0000

    devise:controllers company作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :companies, controllers: {
+  sessions:      'companies/sessions',
+  passwords:     'companies/passwords',
+  registrations: 'companies/registrations'
+}
+  devise_for :users,controllers: {
+  sessions:      'users/sessions',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations'
+}
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "users#top"
