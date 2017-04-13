dfb75b5aeb7daa35c65c4d48bf659a89328845eb
chinamiyamauchi <chinami0370@gmail.com>
2017-04-13 15:13:52 +0000

    ImageUploaderに追加

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,7 +13,29 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+ # リサイズしたり画像形式を変更するのに必要
+  include CarrierWave::RMagick
 
+ # 画像の上限を200pxにする
+   process :resize_to_limit => [200, 200]
+
+  # 保存形式をJPGにする
+  process :convert => 'jpg'
+
+  # サムネイルを生成する設定
+   version :thumb do
+     process :resize_to_fill => [40, 40, gravity = ::Magick::CenterGravity]
+   end
+
+  # jpg,jpeg,gif,pngしか受け付けない
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+ # 拡張子が同じでないとGIFをJPGとかにコンバートできないので、ファイル名を変更
+  def filename
+    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
+  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
@@ -21,10 +43,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
-
-  include CarrierWave::RMagick
-
-   process :resize_to_limit => [100, 100]
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
